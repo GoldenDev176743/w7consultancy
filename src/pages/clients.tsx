b1151c3ed85f00e882cb36d5408b3cfeb837a1db
lily176743 <lily176743@gmail.com>
2023-11-13 15:54:45 +0900

    schedule page frontend complete

--- src/pages/clients.tsx
+++ src/pages/clients.tsx
@@ -1,6 +1,6 @@
 const Clients = () => {
     return (
-        <div className="flex items-center justify-center text-[50px] text-white">This is Client Page</div>
+        <div className="flex items-center justify-center text-[50px] text-white">This is Clientes Page</div>
     );
 }
 
