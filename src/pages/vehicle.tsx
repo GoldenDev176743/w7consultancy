b819744aa8c474e6e2e175b8f4fe184f23444eee
lily176743 <lily176743@gmail.com>
2023-11-13 15:52:16 +0900

    schedule page frontend complete

--- src/pages/vehicle.tsx
+++ src/pages/vehicle.tsx
@@ -1,6 +1,6 @@
 const Vehicle = () => {
     return (
-        <div className="flex items-center justify-center text-[50px] text-white">This is Vehicle Page</div>
+        <div className="flex items-center justify-center text-[50px] text-white">This is Carros Page</div>
     );
 }
 
